44b2f0c3da3b81085320913b64bdfbf5c60029f4
waldrom68 <waldrom68@gmail.com>
2022-05-08 18:12:21 -0300

    empezaondo a reacomodar codigo y a crear .css y .js

---
+++ static/js/menuNev.js
@@ -0,0 +1,24 @@
+{/* <script id="menuNav"> */}
+    /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+    function myFunction() {
+        var x = document.querySelector(".dropdown-content");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+    function menuClose() {
+        var x = document.querySelector(".dropdown-content");
+        x.style.display = "none";
+    }
+// </script> <!-- menuNav -->
+
+{/* <script id="acercaDe"> */}
+    function acercaDe() {
+        element = document.querySelector('.callout');
+        console.log(element)
+        element.style.display='block'
+        menuClose();
+    };
+{/* </script>  <!-- acercaDe --> */}
---
+++ static/js/widget.js
@@ -0,0 +1,138 @@
+
+{/* <script id="myListAction"> */}
+    (function() {
+
+        function initNone() {  
+            // oculto el listado que inicialmente va colapsado
+
+            [].forEach.call(document.querySelectorAll(".myList ~ li.expand > ul > li"), function (e) {
+                e.classList.add("None");
+            }) };
+
+        function upLi(el) {
+
+            if (el.nextElementSibling.className == "") { 
+                // Hice click sobre uno visible, cierro todo
+                initNone();
+
+            } else {  
+                // Cierro todos y MUESTRO solo el elemento clickeado
+                initNone();
+
+                let list = el.parentElement.children
+                for (var i = 1; i < list.length; i++) {
+                    // Muestro el elemento a expandir
+                    list[i].classList.toggle("None");
+                }
+            }
+        }
+        initNone();
+        [].forEach.call(document.querySelectorAll(".myList ~ li.expand ul span"), function(e) {
+            e.addEventListener("click", function() {
+            upLi(e);
+            });
+        })
+    } ());
+// </script>  <!-- treeListAction -->
+
+{/* <script id="rating"> */}
+    (function updateProgress() {
+    [].forEach.call(document.querySelectorAll(".rating"), function(el) {
+        let valor = el.getAttribute("myvalue");
+        el.querySelector(".rtngSide span").innerText = el.getAttribute("mylabel");
+        el.querySelector(".rtngSide.rtngEnd span").innerText = valor
+
+
+        let icos = el.querySelectorAll(".star-container span");
+
+        let index = 0
+        icos.forEach( function(e) {
+        if (index < parseInt(valor))  {
+            e.classList.toggle('checked')
+        }
+        index ++
+        });
+    });
+    } ());
+// </script>  <!-- rating -->
+
+{/* <script id="donuts"> */}
+    function updateDonut(percent, element){
+        /* obtengo valor de los colores desde JS: */
+        let donutStyle = getComputedStyle(document.documentElement);
+        let noProgressColor = donutStyle.getPropertyValue('--progress-bg');
+        let progressColor = donutStyle.getPropertyValue('--progress-color');
+        
+        if (!progressColor) {
+            progressColor = 'red';
+            
+            [].forEach.call(document.querySelectorAll("#section1-chart .item-chart"), function(el) {
+            el.style.setProperty('background-color', progressColor)
+            });
+        };
+        
+        if (!noProgressColor) {
+            noProgressColor = 'initial';
+            [].forEach.call(document.querySelectorAll("#section2-chart .item-chart"), function(el) 
+            {
+                el.style.setProperty('background-color',noProgressColor)
+            });
+        };
+        
+        //var percent = 45;
+        if (percent < 50){
+        offset = (360 / 100) * percent;
+        element.parentNode.querySelector("#section3-chart").style.webkitTransform = "rotate(" + offset + "deg)";
+        element.parentNode.querySelector("#section3-chart .item-chart").style.webkitTransform = "rotate(" + (180 - offset) + "deg)";
+        element.parentNode.querySelector("#section3-chart").style.msTransform = "rotate(" + offset + "deg)";
+        element.parentNode.querySelector("#section3-chart .item-chart").style.msTransform = "rotate(" + (180 - offset) + "deg)";
+        element.parentNode.querySelector("#section3-chart").style.MozTransform = "rotate(" + offset + "deg)";
+        element.parentNode.querySelector("#section3-chart .item-chart").style.MozTransform = "rotate(" + (180 - offset) + "deg)";
+        element.parentNode.querySelector("#section3-chart .item-chart").style.backgroundColor = noProgressColor;
+        
+    } else { 
+        offset = ((360 / 100) * percent) - 180;
+        element.parentNode.querySelector("#section3-chart").style.webkitTransform = "rotate(180deg)";
+        element.parentNode.querySelector("#section3-chart .item-chart").style.webkitTransform = "rotate(" +  offset + "deg)";
+        element.parentNode.querySelector("#section3-chart").style.msTransform = "rotate(180deg)";
+        element.parentNode.querySelector("#section3-chart .item-chart").style.msTransform = "rotate(" +  offset + "deg)";
+        element.parentNode.querySelector("#section3-chart").style.MozTransform = "rotate(180deg)";
+        element.parentNode.querySelector("#section3-chart .item-chart").style.MozTransform = "rotate(" +  offset + "deg)";   
+        element.parentNode.querySelector("#section3-chart .item-chart").style.backgroundColor = progressColor;
+    }
+    element.parentNode.querySelector("span").innerHTML = percent + "%";
+    }
+    
+    function updateSlider(element) {
+        if (element) {
+        var parent = element.parentElement;
+        var thumb = parent.querySelector('.range-slider__thumb'),
+            bar = parent.querySelector('.range-slider__bar'),
+            pct = element.value * ((parent.clientHeight - thumb.clientHeight) / parent.clientHeight);
+        thumb.style.bottom = pct + '%';
+        bar.style.height = 'calc(' + pct + '% + ' + thumb.clientHeight / 2 + 'px)';
+        thumb.textContent = element.value + '%';
+        }
+        updateDonut(element.value, element.parentNode);
+        }
+        (function initAndSetupTheSliders() {
+    
+        [].forEach.call(document.querySelectorAll(".container-chart"), function(el) {
+        let inputs = [].slice.call(el.querySelectorAll('.range-slider input'));
+        let leyend = document.querySelector("#" + el.id + " .center-label span");
+    
+        leyend.innerHTML  = el.getAttribute("aria-label")
+        
+        inputs.forEach(function (input) {
+            input.setAttribute('value', el.getAttribute("aria-value"));
+            updateSlider(input);
+            input.addEventListener('input', function (element) {
+                updateSlider(input);
+            });
+            input.addEventListener('change', function (element) {
+                updateSlider(input);
+            });
+        });
+        });
+    }());
+// </script>  <!-- ts -->
